262b0c6456258a0672604eb4eb9ba1ca5b6428e1
JoshFransix <joshiandersonk69@gmail.com>
2022-07-23 16:31:42 +0100

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,7 +76,7 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
   //   Static rendering for netlify
-  target: 'static',
+  target: 'server',
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
